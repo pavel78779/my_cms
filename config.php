a41169a8dfa5cc0182cd4c14cc24be238c77215d
p.shcherbakov <p.shcherbakov@ceteralabs.com>
2016-02-13 16:55:51 +0300

    Настройка конфига

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 class SConfig{
 	const DB_HOST = 'localhost';
-	const DB_USER = 'admin';
-	const DB_PASS = '123456';
-	const DB_NAME = 'srv55815_webprog';
+	const DB_USER = 'root';
+	const DB_PASS = '';
+	const DB_NAME = 'my_cms';
 	const DB_TABLE_PREFIX = 'kfjsy_';
 
     const HTTP_ERROR_404 = 'Ошибка 404: страница не найдена';
 
-    const SITE_MAIN_URI = 'http://dvig/';
+    const SITE_MAIN_URI = 'http://my_cms/';
 	const SITE_DISABLED = '0';
 	const AJAX_ENABLED = '1';
 
